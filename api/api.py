b03b3786b5e93f36a73d809953769de4b15960cb
mahmoud qunbor <modesq.99q@gmail.com>
2022-12-02 14:51:36 +0000

    done

--- api/api.py
+++ api/api.py
@@ -1,48 +1,34 @@
 from http.server import BaseHTTPRequestHandler
-import requests
 from urllib import parse
+import requests
 
 
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         s = self.path
-        response = ""
-        base_url = "https://restcountries.com/v3.1/"
         url_components = parse.urlsplit(s)
-        query_params = dict(parse.parse_qsl(url_components.query))
-        country = query_params.get("country")
-        capital = query_params.get("capital")
-
-        if not country and not capital:
-            self.send_response(404)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-            self.wfile.write("Please Provide country or captial".encode())
-            return
-
-        if country:
-            url = base_url + f"name/{country}"
-            response = f"The capital of {country} is "
+        print(url_components)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dic = dict(query_string_list)
+        if "country" in dic:
+            country = dic["country"]
+            url = "https://restcountries.com/v3.1/name/"
+            r = requests.get(url + country)
+            data = r.json()
+            message = f"The Capital of {dic['country']} is {data[0]['capital'][0]}"
+        elif "capital" in dic:
+            capital = dic["capital"]
+            url = "https://restcountries.com/v3.1/capital/"
+            r = requests.get(url + capital)
+            data = r.json()
+            print(data)
+            message = f"{dic['capital']} is the Capital of {data[0]['name']['common']}"
         else:
-            url = base_url + f"capital/{capital}"
-            response = f"{capital} is the capital of "
-        try:
-            print(url)
-            r = requests.get(url)
+            message = "Define a country or capital"
 
-            capital_name = r.json()[0]["capital"][0]
-            country_name = r.json()[0]["name"]["common"]
-            print(capital_name, country_name)
-            response = response + capital_name if country else response + country_name
-            self.send_response(200)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-        except Exception as e:
-            print(e)
-            response = f"Sorry, we didnt find results for {country or capital}"
-            self.send_response(404)
-            self.send_header("Content-type", "text/plain")
-            self.end_headers()
-        finally:
-            self.wfile.write(response.encode())
-            return
+        self.send_response(200)
+        self.send_header("Content-type", "text/plain")
+        self.end_headers()
+        self.wfile.write(message.encode())
+
+        return
